cd73dcdce92f6c28455ecfb47ec259b50c525a70
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2020-09-10 15:17:15 +0000

    Check in of base application

---
+++ MyLoginApplication1/MyLoginApplication1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyLoginApplication1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            var loginService = new LoginService();
+            var usernameEntry = this.FindByName<Entry>("UsernameEntry");
+            var passwordEntry = this.FindByName<Entry>("PasswordEntry");
+
+            var loginButton = this.FindByName<Button>("LoginButton");
+            loginButton.IsEnabled = false;
+            try {
+                var result = await loginService.login(usernameEntry.Text, passwordEntry.Text);
+                if (result)
+                {
+                    await DisplayAlert("Login Successfull", "You have been logged in", "Close");
+                }
+                else
+                {
+                    await DisplayAlert("Login Failed", "Login failed", "Close");
+                }
+            } finally {
+                loginButton.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ MyLoginApplication1/MyLoginApplication1/Services.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyLoginApplication1
+{
+    public class LoginService
+    {
+        HttpClient client = new HttpClient();
+
+        public async Task<Boolean> login(string username, string password)
+        {
+            var response = await client.GetAsync($"https://testauth.fastpacetransfer.com:8543/authenticate/fineract-service-bus/Fineract-Login?clientId={WebUtility.UrlEncode(username)}&credential={WebUtility.UrlEncode(password)}");
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
